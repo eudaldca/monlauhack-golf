bb5859dbcbca4f9e6f26b6c9208b4c99e019a2ca
Eudald Cardona <me@eudald.me>
2021-12-20 18:58:19 +0100

    Add student migration

---
+++ database/migrations/2021_12_20_173624_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->text('first_name');
+            $table->text('last_name');
+            $table->text('course');
+            $table->float('average_mark');
+            $table->date('enrollment_date');
+            $table->date('birth_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
