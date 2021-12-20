3ad16aebaaab51ad9cc56e0c178da2551c92d6be
Eudald Cardona <me@eudald.me>
2021-12-20 19:04:52 +0100

    Add Student model definition

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'course',
+        'average_mark',
+        'enrollment_date',
+        'birth_date',
+    ];
+
+    protected $casts = [
+        'enrollment_date'=> 'datetime',
+        'birth_date'=> 'datetime',
+    ];
 }
